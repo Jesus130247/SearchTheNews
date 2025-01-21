7c41d2e069cd0558fc1963c960207486b21c2a5f
jesus <leonardo.monty@hotmail.com>
2025-01-21 16:15:12 +1100

    final touch ups, removing excess code and comments

--- App.js
+++ App.js
@@ -15,7 +15,7 @@ export default function App() {
     setLoading(true)
     try {
       const data = await fetch(`https://newsapi.org/v2/everything?q=${search}&apiKey=183daca270264bad86fc5b72972fb82a`).then(res => res.json())
-      setArticles(data.articles.filter(data => data.title !== '[Removed]')) // handeling for removed items
+      setArticles(data.articles.filter(data => data.title !== '[Removed]'))
     } catch (error) {
       alert('Error Fetching articles.')
     } finally {
@@ -35,15 +35,11 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Image source={{uri: 'https://thumbs.dreamstime.com/b/newspaper-stack-vector-illustration-news-paper-pile-front-page-top-view-abstract-text-articles-headlines-world-news-70078112.jpg'}} 
-      style={{
-        width: 200,
-        height: 200,
-        borderRadius: 10,
-        marginBottom: 10,
-      }}/>
+      style={styles.image}/>
+      <Text>Seach anything you want</Text>
       <TextInput 
       style={styles.input}
-      placeholder='Search News'
+      placeholder='Type your search...'
       value={search}
       onChangeText={setSearch}
       />
@@ -61,12 +57,7 @@ export default function App() {
               <Text>Date Published: {item.publishedAt}</Text>
               <Text>URL: {item.url}</Text>
               <Image source={{uri: `${item.urlToImage}`}} 
-              style={{
-                width: 400,
-                height: 400,
-                resizeMode: 'contain',
-                borderRadius: 10,
-              }}/>
+              style={styles.image}/>
               <Text>url to image: {item.urlToImage}</Text>
             </View>
         )}
@@ -112,4 +103,10 @@ const styles = StyleSheet.create({
   title: {
     fontWeight: 'bold'
   },
+  image: {
+    width: 200,
+    height: 200,
+    borderRadius: 10,
+    marginBottom: 10,
+  }
 });
